1b5e189155d0772fe29ada5d8d4e7454ff22c769
BencsikG <bgeri91@gmail.com>
2022-05-31 08:24:10 +0200

    Added README content

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # manim-Gearbox
-To be written...
+This is a plugin for Manim that enables you to draw realistic looking gears and mechanisms.
+So far only involute gears are supported, with inside and outside gears.
+Planned further development:
+- Rack and pinion
+- Cycloid gears, cycloid rack
+- Sliced gears
+- Animation helpers
+
+**Example**
+```py
+class gear_example(Scene):
+    def construct(self):
+        # 20 tooth gear with module = 0.2
+        gear1=Gear(0.2, 20, stroke_opacity=0, fill_color=WHITE,fill_opacity=1)
+        # 40 tooth gear with module = 0.2
+        gear2=Gear(0.2, 40, stroke_opacity=0, fill_color=RED, fill_opacity=1)
+
+        # shifting gears away from center
+        gear2.shift(gear2.rp * RIGHT)
+        gear1.shift(-gear1.rp * RIGHT)
+
+        self.add(gear1)
+        self.add(gear2)
+        self.play(Rotate(gear1, PI / 10, rate_func=linear), Rotate(gear2, - PI / 10 / 2, rate_func=linear), run_time=4)
+		
+```
+![involute_gear_example](/media/involute_gear_example.gif)
--- tests/test_manim_gearbox.py
+++ tests/test_manim_gearbox.py
@@ -7,24 +7,15 @@ def test_version():
 
 class gear_test(Scene):
     def construct(self):
-        gear1=Gear(0.2,20,stroke_color=WHITE,stroke_width=1.5, stroke_opacity=0, fill_color=WHITE,fill_opacity=0.5)
-        gear2=Gear(0.2,40,
-                   stroke_color=RED,stroke_width=1.5,stroke_opacity=0,
-                   inner_teeth=True,fill_color=RED,fill_opacity=1,h_a=0.7)
-        # gear1.rotate(-gear1.rp_tooth_ofs)
-        # gear2.rotate(-gear2.rp_tooth_ofs)
-        circ = Circle(gear2.ra+0.3)
+        # 20 tooth gear with module = 0.2
+        gear1=Gear(0.2, 20, stroke_opacity=0, fill_color=WHITE,fill_opacity=1)
+        # 40 tooth gear with module = 0.2
+        gear2=Gear(0.2, 40, stroke_opacity=0, fill_color=RED, fill_opacity=1)
 
-        gear3 = Difference(circ,gear2)
-        gear2.shift((gear2.rp + gear1.rp) * RIGHT)
-        gear3.shift((-gear2.rp+gear1.rp*0)*RIGHT)
-        gear3.set_fill(color=RED,opacity=1)
-        gear3.set_stroke(opacity=0)
-        gear1.shift(-gear1.rp*RIGHT)
+        # shifting gears away from center
+        gear2.shift(gear2.rp * RIGHT)
+        gear1.shift(-gear1.rp * RIGHT)
 
         self.add(gear1)
-        # self.add(gear2)
-        self.add(gear3)
-
-        # self.play(Rotate(gear1, PI / 10, rate_func=linear), Rotate(gear2, -PI / 10 / 3, rate_func=linear), run_time=4)
-        self.play(Rotate(gear1, PI / 10, rate_func=linear), Rotate(gear3, PI / 10 / 2, rate_func=linear), run_time=4)
+        self.add(gear2)
+        self.play(Rotate(gear1, PI / 10, rate_func=linear), Rotate(gear2, - PI / 10 / 2, rate_func=linear), run_time=4)
